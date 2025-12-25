aaa4b0a3ea82070a3ed138ad134cc5e910f296d2
flj33 <arkadiusz.jusko@gmail.com>
2025-12-25 15:42:14 +0100

    Configure base path for GitHub Pages
    Set base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/stolarkapinkowski/",   // ważne dla GitHub Pages
+
   server: {
     host: "::",
     port: 8080,
